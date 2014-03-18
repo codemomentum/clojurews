870dec65357bfa9bed5e0c6da2814a5da3b09cb4
Halit Olali <halitolali@gmail.com>
2014-03-18 10:42:25 +0200

    initial import

---
+++ src/clojurews/core.clj
@@ -0,0 +1,6 @@
+(ns clojurews.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojurews/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojurews.core-test
+  (:require [clojure.test :refer :all]
+            [clojurews.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
